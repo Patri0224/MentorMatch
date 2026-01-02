b955e21dd5dcecb95cb194a81b7c3cc61f86d36e
felitti <ffelitti@studenti.uninsubria.it>
2026-01-02 15:47:52 +0100

    test server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
-const app = require('./src/app');
+const app = require('./app.js');
+
+console.log(process.env.DATABASE_URL);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server avviato sulla porta ${PORT}`);
-  });
+
