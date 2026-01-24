1d1e023f56ea74982f3cea6c84c4a1da941aab36
felitti <ffelitti@studenti.uninsubria.it>
2026-01-24 16:37:11 +0100

    fix (spero l'ultimo)

--- backend/routes/message_routes.js
+++ backend/routes/message_routes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { postMessages, getMessages, markAsRead, getChatHistory } from '../controllers/message_controller.js';
+import { postMessage, getMessages, markAsRead, getChatHistory } from '../controllers/message_controller.js';
 import { authenticateToken } from '../src/middleware/auth_middleware.js';
 
 const router = express.Router();
 
-router.post('/post-message', authenticateToken, postMessages);
+router.post('/post-message', authenticateToken, postMessage);
 router.post('/get-messages', authenticateToken, getMessages);
 router.post('/get-chat-history', authenticateToken, getChatHistory);
 router.post('/mark-as-read', authenticateToken, markAsRead);
