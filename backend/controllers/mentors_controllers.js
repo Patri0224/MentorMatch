3db04f3830a88a067fecdaee8c926139d53eb819
Patrizio <106958889+Patri0224@users.noreply.github.com>
2026-01-24 21:45:07 +0100

    dsa

--- backend/controllers/mentors_controllers.js
+++ backend/controllers/mentors_controllers.js
@@ -23,7 +23,7 @@ export const listMentors = async (req, res) => {
                     SELECT 1
                     FROM sessions s
                     WHERE s.mentor_id = m.id
-                      AND s.status = 'available'
+                      AND s.available = 'available'
                       -- Filtro Data (se presente)
                       AND ($5::TIMESTAMP IS NULL OR s.start_time <= $5)
                       AND ($6::TIMESTAMP IS NULL OR s.end_time >= $6)
