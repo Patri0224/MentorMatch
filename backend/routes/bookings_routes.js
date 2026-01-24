ab7c50c5b25f89c442812b48a31b3a2c322a20f4
felitti <ffelitti@studenti.uninsubria.it>
2026-01-24 16:30:04 +0100

    fix dell'h

--- backend/routes/bookings_routes.js
+++ backend/routes/bookings_routes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 
-import { autenticateToken } from '../src/middleware/auth_middleware.js';
+import { authenticateToken } from '../src/middleware/auth_middleware.js';
 import { cancelBooking, createCheckoutBooking, getUserBookings } from '../controllers/bookings_controller.js';
 
 const router = express.Router();
 
-router.post('/checkout', autenticateToken, createCheckoutBooking);
-router.post('/user_bookings', autenticateToken, getUserBookings);
-router.post('/cancel_booking', autenticateToken, cancelBooking);
+router.post('/checkout', authenticateToken, createCheckoutBooking);
+router.post('/user_bookings', authenticateToken, getUserBookings);
+router.post('/cancel_booking', authenticateToken, cancelBooking);
 export default router;
