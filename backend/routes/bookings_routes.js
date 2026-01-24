4cc4dbc7fb266337b6fad82c508069a7eb955c47
felitti <ffelitti@studenti.uninsubria.it>
2026-01-24 16:10:36 +0100

    fix controller

--- backend/routes/bookings_routes.js
+++ backend/routes/bookings_routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import { autenticateToken } from '../src/middleware/auth_middleware.js';
-import { cancelBooking, createCheckoutBooking, getUserBookings } from '../controllers/bookings_controllers.js';
+import { cancelBooking, createCheckoutBooking, getUserBookings } from '../controllers/bookings_controller.js';
 
 const router = express.Router();
 
