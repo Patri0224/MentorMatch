42e195bebfcb08068953c4688cbfbbeaa98ef327
Patrizio <106958889+Patri0224@users.noreply.github.com>
2026-01-24 18:55:52 +0100

    aaaa

--- frontend/public/src/api/api.js
+++ frontend/public/src/api/api.js
@@ -358,8 +358,8 @@ const ApiService = {
      */
     async getMentorById(id) {
         try {
-            debugger;
             const response = await fetch(`${API_BASE_URL} / mentors / get-mentor / ${id}`);
+            debugger;
             if (!response.ok) throw new Error('Profilo non trovato ' + JSON.stringify(response, null, 2));
             return await response.json();
         } catch (error) {
